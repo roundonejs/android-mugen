dcb6be39d699aa3cf64e1b34dba31d68560f1b0b
Carlson Santana Cruz <carlsonsantana@gmail.com>
2023-12-05 00:06:23 -0300

    Simplify build process

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,5 +1,5 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := x86
+APP_ABI := armeabi-v7a x86 arm64-v8a x86_64
 APP_STL := stlport_static
 
 APP_PLATFORM := android-9
---
+++ app/src/main/jni/dosbox/config.h
@@ -0,0 +1,11 @@
+#ifdef __i386__
+  #include "config_x86.h"
+#elif __x86_64__
+  #include "config_x86_64.h"
+#elif __arm__
+  #include "config_armeabi_v7a.h"
+#elif __aarch64__
+  #include "config_arm64_v8a.h"
+#else
+  #error "Architeture without configure file."
+#endif
